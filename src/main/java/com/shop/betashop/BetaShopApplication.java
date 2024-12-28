334fcb1e6c437fb71e5fc49156a2e0274c54fc20
hansae <mugy2869@gmail.com>
2024-12-28 23:02:00 +0900

    세팅 커밋

---
+++ src/main/java/com/shop/betashop/BetaShopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.betashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetaShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/betashop/BetaShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shop.betashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//@SpringBootTest(properties = {
+//        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+//})
+class BetaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
