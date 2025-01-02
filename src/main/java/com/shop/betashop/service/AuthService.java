a5164ca3101041500fc8e9ff2c31e62ec968e7c1
hansae <mugy2869@gmail.com>
2025-01-03 00:57:43 +0900

    feat(auth): JWT 토큰을 사용한 로그인 기능 구현
    다음 작업을 적용 중 :
    - 사용자 인증을 위한 로그인 API 구현
    - 인증된 사용자에 대한 JWT 토큰 생성 기능 추가
    - 이메일 또는 전화번호로 사용자를 조회하는 UserRepository 메서드 추가
    - JWT 클레임을 활용한 역할 기반 접근 제어 적용

--- src/main/java/com/shop/betashop/service/AuthService.java
+++ src/main/java/com/shop/betashop/service/AuthService.java
@@ -4,23 +4,33 @@ import com.shop.betashop.dto.LoginRequest;
 import com.shop.betashop.dto.LoginResponse;
 import com.shop.betashop.model.User;
 import com.shop.betashop.repository.UserRepository;
+import com.shop.betashop.util.JwtUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
     private static final Logger logger = LoggerFactory.getLogger(AuthService.class);
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
+    /**
+     * 로그인 검증 로직 생성
+     * @param loginRequest
+     * @return
+     */
     public LoginResponse authenticate(LoginRequest loginRequest){
-        LoginResponse loginResponse = new LoginResponse("");
-
         String userId = loginRequest.getUserId();
+        String password = loginRequest.getPassword();
 
         User user;
         if(isEmail(userId)){
@@ -33,18 +43,30 @@ public class AuthService {
             throw new IllegalArgumentException("Invalid user id");
         }
 
+        if(!passwordEncoder.matches(password, user.getPassword())){
+            throw new IllegalArgumentException("Invalid user id");
+        }
 
+        LoginResponse loginResponse = new LoginResponse(jwtUtil.generateToken(user));
 
         return loginResponse;
     }
 
-    // 이메일인지 확인하는 메서드
+    /**
+     * 이메일인지 확인
+     * @param input
+     * @return
+     */
     private boolean isEmail(String input) {
         String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
         return input.matches(emailRegex);
     }
 
-    // 전화번호인지 확인하는 메서드
+    /**
+     * 전화번호인지 확인
+     * @param input
+     * @return
+     */
     private boolean isPhoneNumber(String input) {
         String phoneRegex = "^\\d{10,15}$"; // 10~15 자리 숫자로 구성된 전화번호
         return input.matches(phoneRegex);
--- src/main/java/com/shop/betashop/util/JwtUtil.java
+++ src/main/java/com/shop/betashop/util/JwtUtil.java
@@ -1,11 +1,37 @@
 package com.shop.betashop.util;
 
+import com.shop.betashop.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
-    private String SECRET_KEY = "betashop";
+    @Value("${spring.jwt.secret}")
+    private String SECRET_KEY;
     private final long TOKEN_TIME = 60 * 60 * 100L; //10분
+
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", user.getUserName());
+        claims.put("email", user.getEmail());
+        claims.put("role", user.getRole());
+        claims.put("Phone",user.getPhone());
+
+        return Jwts.builder()
+                .setClaims(claims) // Payload - 사용자 정보
+                .setSubject(user.getUserName()) // 주제 설정 (보통 사용자 ID)
+                .setIssuedAt(new Date()) // 토큰 생성 시간
+                .setExpiration(new Date(System.currentTimeMillis() + TOKEN_TIME)) // 토큰 만료 시간
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY) // 서명 알고리즘 및 비밀 키
+                .compact(); // 토큰 생성
+    }
+
 
 
 }
